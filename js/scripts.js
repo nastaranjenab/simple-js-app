ff49456c930851e98fea37e08bc08556831c7c00
Nastaranjenab <Nastaran.jenabali@yahoo.com>
2022-02-13 00:57:18 +0100

    added Pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let pokemanList=[
+let pokemonList=[
     {name:'Squirtle',
     height:0.5,
     types:['water']
@@ -16,3 +16,9 @@ height:0.4,
 types:['Normal']
 },
 ]
+for (let i=0; i < pokemonList.length; i++){
+    if(pokemonList[i].height > 1){
+      document.write(pokemonList[i].name + "( height : " + pokemonList[i].height + " )" + "Wow it`s a big pokemon!!!")
+    }else {
+    document.write(pokemonList[i].name + "( height : " + pokemonList[i].height + " )")
+  }}
