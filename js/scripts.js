ffcd957f1efab17f693e46066da572ca2ddcf984
Nastaranjenab <Nastaran.jenabali@yahoo.com>
2022-02-19 01:06:04 +0100

    added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,48 @@ height:0.4,
 types:['Normal']
 },
 ]
-pokemonList.forEach(function(item) {
-  console.log(item);
-});
+
+
+let pokemonRepository = (function () {
+
+let pokemonList=[
+{name:'Squirtle',
+height:0.5,
+types:['water']
+},
+{name:'Watchdog',
+height:1.1,
+types:['Normal']
+},
+{name:'Tepig',
+height:0.5,
+types:['Fire']
+},
+{name:'Minccino',
+height:0.4,
+types:['Normal']
+},
+]
+
+function getAll () {
+  return pokemonList;
+}
+function add (pokemon) {
+  pokemonList.push(pokemon);
+}
+
+  return {
+    getAll: getAll,
+    add: add
+  }
+
+  })()
+
+  pokemonRepository.getAll().forEach(function(pokemon){
+    document.write(pokemon.name + " with type: (" + pokemon.type + ") " + "and" + " (height :" + pokemon.height + ") <br>")
+    });
+    
+    
+    console.log(pokemonRepository.getAll());
+    pokemonRepository.add({ name: 'Mareep' });
+    console.log(pokemonRepository.getAll());
