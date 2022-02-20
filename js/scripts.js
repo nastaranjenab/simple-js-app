2b5f3d6fc10cc92b6686e8663b766c793ea4d4c4
Nastaranjenab <Nastaran.jenabali@yahoo.com>
2022-02-20 19:55:11 +0100

    corrected IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,48 @@
-let pokemonList=[
-    {name:'Squirtle',
-    height:0.5,
-    types:['water']
-},
-{name:'Watchdog',
-height:1.1,
-types:['Normal']
-},
-{name:'Tepig',
-height:0.5,
-types:['Fire']
-},
-{name:'Minccino',
-height:0.4,
-types:['Normal']
-},
-]
+let pokemonRepository = (function() {
 
+  let pokemonList = [{
+          name: 'Squirtle',
+          height: 0.5,
+          types: ['water']
+      },
+      {
+          name: 'Watchdog',
+          height: 1.1,
+          types: ['Normal']
+      },
+      {
+          name: 'Tepig',
+          height: 0.5,
+          types: ['Fire']
+      },
+      {
+          name: 'Minccino',
+          height: 0.4,
+          types: ['Normal']
+      },
+  ]
 
-let pokemonRepository = (function () {
-
-let pokemonList=[
-{name:'Squirtle',
-height:0.5,
-types:['water']
-},
-{name:'Watchdog',
-height:1.1,
-types:['Normal']
-},
-{name:'Tepig',
-height:0.5,
-types:['Fire']
-},
-{name:'Minccino',
-height:0.4,
-types:['Normal']
-},
-]
-
-function getAll () {
-  return pokemonList;
-}
-function add (pokemon) {
-  pokemonList.push(pokemon);
-}
-
-  return {
-    getAll: getAll,
-    add: add
+  function getAll() {
+      return pokemonList;
   }
 
-  })()
+  function add(pokemon) {
+      pokemonList.push(pokemon);
+  }
 
-  pokemonRepository.getAll().forEach(function(pokemon){
-    document.write(pokemon.name + " with type: (" + pokemon.type + ") " + "and" + " (height :" + pokemon.height + ") <br>")
-    });
-    
-    
-    console.log(pokemonRepository.getAll());
-    pokemonRepository.add({ name: 'Mareep' });
-    console.log(pokemonRepository.getAll());
+  return {
+      getAll: getAll,
+      add: add
+  }
+
+})()
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  document.write(pokemon.name + " with type: (" + pokemon.types + ") " + "and" + " (height :" + pokemon.height + ") <br>")
+});
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({
+  name: 'Mareep'
+});
+console.log(pokemonRepository.getAll());
